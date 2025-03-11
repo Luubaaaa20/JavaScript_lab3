96655527da8c08548f87a679802d765e0c5af652
Luubaaaa20 <114647989+Luubaaaa20@users.noreply.github.com>
2025-03-11 14:46:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 (function() {
     const names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-    for (let i = 0; i < names.length; i++) {
-        const name = names[i];
+    names.forEach(name => {
         if (name.charAt(0).toLowerCase() === "j") {
             window.speakGoodBye.speak(name);
         } else {
             window.speakHello.speak(name);
         }
-    }
+    });
 
-    console.log("\nДодатковий функціонал: Імена з довжиною <= 4 символів:");
+    console.log("\nДодатковий функціонал: розподіл імен за парністю довжини:");
+    const evenNames = [];
+    const oddNames = [];
+
     names.forEach(name => {
-        if (name.length <= 4) {
-            console.log(name);
+        if (name.length % 2 === 0) {
+            evenNames.push(name);
+        } else {
+            oddNames.push(name);
         }
     });
+
+    console.log("Імена з парною кількістю символів: " + evenNames.join(", "));
+    console.log("Імена з непарною кількістю символів: " + oddNames.join(", "));
 })();
